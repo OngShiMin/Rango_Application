811c568118fc862cdcadafd5446ea05400f3ea70
OngShiMin <46679883+OngShiMin@users.noreply.github.com>
2019-02-10 22:47:39 +0000

    Add files via upload

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
     }
 }
 
+# Password hashing functions
+# https://docs.djangoproject.com/en/1.9/topics/auth/passwords/#how-django-stores-passwords
+PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+  ]
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -95,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -124,6 +131,7 @@ USE_TZ = True
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
+LOGIN_URL ='/rango/login/'
 
 # Media Files
 MEDIA_ROOT = MEDIA_DIR
