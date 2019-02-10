7e9e82aae4c4bc316b4ed34cf65604d8c8ccacd9
OngShiMin <46679883+OngShiMin@users.noreply.github.com>
2019-02-10 18:44:01 +0000

    Add files via upload

---
+++ tango_with_django_project/rango/templatetags/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 17:14:32 2019
+
+@author: Shimin
+"""
+
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -16,6 +16,8 @@ def index(request):
 
 def about(request):
    # return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+   print(request.method)
+   print(request.user)
    return render(request, 'rango/about.html', {})
 
 
